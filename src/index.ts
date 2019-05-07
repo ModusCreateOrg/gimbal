03fc95a237c6a3b8a8b3a0413d3e56f94e040e17
Mitchell Simoens <mitchellsimoens@gmail.com>
2019-05-07 09:46:41 -0400

    Get version from package.json

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import fs from 'fs';
 import path from 'path';
 import Command, { preparseOptions } from '@/command';
 import Config from '@/config';
-import { CHILD_GIMBAL_PROCESS } from '@/utils/constants';
 import processJobs from '@/config/jobs';
+import { CHILD_GIMBAL_PROCESS } from '@/utils/constants';
+import { readFile } from '@/utils/fs';
 
 (async (): Promise<void> => {
   if (!process.env[CHILD_GIMBAL_PROCESS]) {
@@ -14,8 +15,12 @@ import processJobs from '@/config/jobs';
     console.log(gimbal);
   }
 
+  // cannot import package.json as it is not within the rootDir (aka, src/)
+  const rawPackageJson = await readFile(path.join(__dirname, '../package.json'), 'utf8');
+  const packageJson = JSON.parse(rawPackageJson);
+
   program
-    .version('1.0.0')
+    .version(packageJson.version)
     .description('A CLI tool for monitoring web performance in modern web projects')
     // global options all command will receive
     .option('--cwd [dir]', 'The directory to work in. Defaults to where the command was executed from.', process.cwd())
