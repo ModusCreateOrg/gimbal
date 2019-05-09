253ffb630a55ae54a53728625ba3a63dbc84a1a5
Mitchell Simoens <mitchellsimoens@gmail.com>
2019-05-09 14:24:16 -0400

    Resolve a plugin from the cwd if it exists

--- src/config/resolver/index.ts
+++ src/config/resolver/index.ts
@@ -1,3 +1,5 @@
+import { existsSync } from 'fs';
+import path from 'path';
 import { resolvePath } from '@/utils/fs';
 
 const IS_FILE_RE = /^[./]/;
@@ -6,28 +8,32 @@ const IS_USER_HOME_RE = /^~/;
 
 const GIMBAL_ROOT = resolvePath(__dirname, '../..');
 
-const resolver = (path: string, dir: string): string => {
+const resolver = (requested: string, dir: string): string => {
   // if path starts with `~` then this is a filesystem path
   // that needs to be mapped to the user's home dir. This
   // should be handled by nodejs in a platform agnostic
   // way so we shouldn't need to do anything.
-  if (path.match(IS_USER_HOME_RE)) {
-    return path;
+  if (requested.match(IS_USER_HOME_RE)) {
+    return requested;
   }
 
   // if a path starts with `/` or `.` then this is a
   // filesystem path.
-  if (path.match(IS_FILE_RE)) {
-    return resolvePath(dir, path);
+  if (requested.match(IS_FILE_RE)) {
+    return resolvePath(dir, requested);
   }
 
   // if a path starts with '@/' then it should be looked up
   // as a gimbal internal plugin
-  if (path.match(IS_GIMBAL_RE)) {
-    return resolvePath(GIMBAL_ROOT, path.substr(2));
+  if (requested.match(IS_GIMBAL_RE)) {
+    return resolvePath(GIMBAL_ROOT, requested.substr(2));
   }
 
-  return path;
+  // see if it's a node module install on the cwd
+  // where gimbal is running on
+  const cwdRelativeNodeModule = path.join(dir, 'node_modules', requested);
+
+  return existsSync(cwdRelativeNodeModule) ? cwdRelativeNodeModule : requested;
 };
 
 export default resolver;
