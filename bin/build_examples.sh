4caca1ef539f311d132c108676e9094c12b62a18
Mitchell Simoens <mitchellsimoens@gmail.com>
2019-06-26 10:06:28 -0400

    feat(ci): test examples for commits/PRs (#129)
    * feat(ci): test examples for commits/PRs
    
    * fix(ci): fix circleci config
    
    * fix(ci): use image with browsers

---
+++ bin/build_examples.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Set bash unofficial strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+# Set DEBUG to true for enhanced debugging: run prefixed with "DEBUG=true"
+${DEBUG:-false} && set -vx
+# Credit to https://stackoverflow.com/a/17805088
+# and http://wiki.bash-hackers.org/scripting/debuggingtips
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+# Credit to http://stackoverflow.com/a/246128/424301
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR="$( cd "${DIR}/.." && pwd )"
+EXAMPLES_DIR="$BASE_DIR/examples"
+
+for file in $EXAMPLES_DIR/*; do
+  cd $file
+
+  yarn
+  yarn build
+done
