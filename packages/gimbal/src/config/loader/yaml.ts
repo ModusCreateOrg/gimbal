57a8aad7d58fd9052c8fa48f7d499fa451b1d21e
Mitchell Simoens <mitchellsimoens@gmail.com>
2019-07-15 09:21:20 -0400

    feat(config): allow variable replacement in yaml config (#138)
    * feat(config): allow variable replacement in yaml config
    
    * Update deps

--- packages/gimbal/src/config/loader/yaml.ts
+++ packages/gimbal/src/config/loader/yaml.ts
@@ -1,9 +1,24 @@
+import env from '@modus/gimbal-core/lib/utils/env';
 import { readFile } from '@modus/gimbal-core/lib/utils/fs';
 import yaml from 'js-yaml';
 import { Config } from '@/typings/config';
 
+const VAR_RE = /\${(.+):(.+)}/gm;
+
 const yamlLoader = async (file: string): Promise<Config> => {
-  const doc = await readFile(file, 'utf8');
+  const source = await readFile(file, 'utf8');
+  // this will allow for variable replacement. The following are the supported types and formats:
+  // - ${env:SOME_VAR}
+  //    The type is "env" which will lookup "SOME_VAR" on process.env. If the var is not found
+  //    then the text will not be replaced.
+  // If the type is unrecognized, the text will not be replaced.
+  const doc = source.replace(VAR_RE, (text: string, type: string, prop: string): string => {
+    if (type === 'env') {
+      return env(prop, text);
+    }
+
+    return text;
+  });
 
   return yaml.safeLoad(doc, {
     filename: file,
