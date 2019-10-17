26a21b2e2846bccabf5ac7723f0ea54605eebd1f
Mitchell Simoens <mitchellsimoens@gmail.com>
2019-10-17 16:30:42 -0400

    fix failing spec

--- packages/gimbal-core/src/utils/colors.spec.ts
+++ packages/gimbal-core/src/utils/colors.spec.ts
@@ -1,8 +1,21 @@
 import { sectionHeading, successOrFailure } from './colors';
 
+const ORIG_ENV = process.env;
+
+beforeEach((): void => {
+  process.env = {};
+});
+
+afterEach((): void => {
+  process.env = { ...ORIG_ENV };
+});
+
 describe('@modus/gimbal-core/utils/colors', (): void => {
   describe('sectionHeading', (): void => {
     it('should bold text', (): void => {
+      // in a CI, force the color
+      process.env['FORCE_COLOR'] = 'true';
+
       const ret = sectionHeading('this should be bold');
 
       expect(ret).toBe('[1mthis should be bold[22m');
@@ -11,12 +24,18 @@ describe('@modus/gimbal-core/utils/colors', (): void => {
 
   describe('successOrFailure', (): void => {
     it('should be green for success', (): void => {
+      // in a CI, force the color
+      process.env['FORCE_COLOR'] = 'true';
+
       const ret = successOrFailure('this should be green', true);
 
       expect(ret).toBe('[32mthis should be green[39m');
     });
 
     it('should be red for failure', (): void => {
+      // in a CI, force the color
+      process.env['FORCE_COLOR'] = 'true';
+
       const ret = successOrFailure('this should be red', false);
 
       expect(ret).toBe('[31mthis should be red[39m');
