bb936a7d0b29be8651cf07a2da8df4c7969c5f82
Mitchell Simoens <mitchellsimoens@gmail.com>
2019-10-18 12:57:23 -0400

    Get lighthouse to work

--- packages/gimbal/src/module/chrome/default-config.ts
+++ packages/gimbal/src/module/chrome/default-config.ts
@@ -1,3 +1,4 @@
+import envOrDefault from '@modus/gimbal-core/lib/utils/env';
 import { LaunchOptions } from 'puppeteer';
 
 // can accept anything from: https://github.com/GoogleChrome/puppeteer/blob/v1.14.0/docs/api.md#puppeteerlaunchoptions
@@ -5,5 +6,11 @@ const defaultConfig: LaunchOptions = {
   // args useful for headless CI instances
   args: ['--no-sandbox', '–-disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
 };
+
+const CHROME_BIN = envOrDefault('CHROME_BIN');
+
+if (CHROME_BIN) {
+  defaultConfig.executablePath = CHROME_BIN;
+}
 
 export default defaultConfig;
