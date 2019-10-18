79ec8d942b4d8c17ce863e8d669dc65abda7b7fa
Mitchell Simoens <mitchellsimoens@gmail.com>
2019-10-18 14:24:13 -0400

    update some files

---
+++ bin/test/run_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This is only to test building the docker image locally
+
+# Set bash unofficial strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+# Set DEBUG to true for enhanced debugging: run prefixed with "DEBUG=true"
+${DEBUG:-false} && set -vx
+# Credit to https://stackoverflow.com/a/17805088
+# and http://wiki.bash-hackers.org/scripting/debuggingtips
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+# Credit to http://stackoverflow.com/a/246128/424301
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR="$( cd "${DIR}/../.." && pwd )"
+
+docker run -v "$BASE_DIR/examples/react":/react -w /react moduscreate/gimbal:local gimbal
